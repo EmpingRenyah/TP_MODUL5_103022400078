7250ee007e5523828a9632583bd8600ca585c70c
EmpingRenyah <viha.el2629@gmail.com>
2026-03-29 21:41:18 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+public class HaloGeneric
+{
+    public static void SapaUser(string name)
+    {
+        Console.WriteLine("Hello, User " + name);
+    }
+
+    public static void Main(string[] args)
+    {
+        SapaUser("Vincent");
+    }
+}
