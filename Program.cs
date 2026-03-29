6882d7317e69e62f6548473c50fdd2b44b16ea1a
EmpingRenyah <viha.el2629@gmail.com>
2026-03-29 22:00:06 +0700

    commit message

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Reflection.Metadata;
+
 public class HaloGeneric
 {
     public static void SapaUser(string name)
@@ -9,5 +11,22 @@ public class HaloGeneric
     public static void Main(string[] args)
     {
         SapaUser("Vincent");
+        DataGeneric<long> data = new DataGeneric<long>(103022400078);
+        data.PrintData();
+    }
+}
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
     }
 }
